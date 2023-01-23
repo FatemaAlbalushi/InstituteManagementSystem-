79296c1f788d3f93b837c2ec283ebc40caafe76c
FatemaAlbalushi <fatoomsaid188@gmail.com>
2023-01-23 08:32:26 +0400

    Add the files

---
+++ src/main/java/InstituteManagementSystem.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAP-10
+ *
+ */
+public class InstituteManagementSystem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
